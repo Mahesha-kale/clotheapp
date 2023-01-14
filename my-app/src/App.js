e8374e5d18a99a0295bb3c5bc6dfa9a88abdac2d
Mahesh Kale <maheshkale459@gmail.com>
2023-01-14 10:32:09 +0530

    removed all routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,40 +6,18 @@ import {Wash} from './MyComponents/Wash';
 import {Wardrobe} from './MyComponents/Wardrobe';
 import {Info} from './MyComponents/Info';
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  
-} from "react-router-dom";
+
 
 
 function App() {
   
   return (
     <>
-    <Router basename='/clotheapp'>
-    <Header title="My List" searchBar={false}/>
-    <Switch>
-          
-          <Route exact path="/clotheapp" component={()=>
-            (
-              <>
-              <h1>Hello !!</h1>
-              </>
-            )
-          
-          }/>
-          <Route exact path="/info" component={Info}/>
-          <Route exact path="/wash" component={Wash}/>
-          <Route exact path="/wardrobe" component={Wardrobe}/>
-            
-    </Switch>
-    
-    
-    
+    <Header title="My List" searchBar={false}/> 
+    <Info/> 
+    <Wash/> 
+    <Wardrobe/>     
     <Footer/>
-    </Router>
     </>
   );
 }
